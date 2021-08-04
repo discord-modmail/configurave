3c29ab414c13a0c9886f0a4a5fc4d7f352b13fd0
Bast <bast@fastmail.com>
2021-08-03 20:55:21 -0700

    Add default tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,13 +1,39 @@
 import unittest
 
+from configurave import ConfigEntry as ce
+from configurave import make_config
+
 """
-To ensure workflows succeed, this is a temporary test file for github actions.
+Apply basic tests to configurave config loading.
 """
 
 
-class TestTest(unittest.TestCase):
-    """Demo test."""
+class TestConfigurave(unittest.TestCase):
+    """General tests."""
 
-    def test_test(self):
-        """True is true."""
-        self.assertTrue(True)
+    def test_everything(self):
+        """We can create and load a configuration from our test config folder."""
+        # TODO: split this up and write better tests
+
+        @make_config(
+            sources=[  # in order of priority
+                "tests/test-config/config.toml",
+                # "ENV",  # Temporarily disabled until we get optional dotenv test up and running
+            ]
+        )
+        class Config:
+            """The test configuration for configurave."""
+
+            root_url: str = ce(
+                comment="The root url configuration for the application",
+                description="A long ass multiline description goes here about all the options"
+                " you could potentially decide upon using.",
+            )
+
+        c = Config()
+        c.load()
+
+        self.assertIn("root_url", str(c._crve_configs))
+        self.assertEqual(c.root_url, "test url")
+
+        # print("Defaults toml file:\n" + c.generate_defaults())
