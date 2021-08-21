b5a6d146e27e4912763b22fbd473e5b885d082eb
onerandomusername <genericusername414+git@gmail.com>
2021-08-20 23:53:22 -0400

    tests: add additional tests

--- tests/test_configurave.py
+++ tests/test_configurave.py
@@ -8,6 +8,31 @@ Apply basic tests to configurave config loading.
 """
 
 
+@pytest.mark.dependency(name="load_dotenv")
+def test_validate_sources_env():
+    """Test env sources are validated properly."""
+    try:
+        from dotenv import load_dotenv
+    except ImportError:
+        pytest.skip("dotenv not installed")
+
+    from configurave import validate_sources
+
+    validate_sources(["ENV"])
+
+
+def test_validate_sources_env_dotenv_not_installed():
+    """Test env source errors properly when dotenv is not installed."""
+    import configurave
+    from configurave import load_dotenv, validate_sources
+
+    if load_dotenv is not None:
+        pytest.skip("dotenv is installed")
+
+    with pytest.raises(ConfigError):
+        validate_sources(["ENV"])
+
+
 def test_everything():
     """We can create and load a configuration from our test config folder."""
     # TODO: split this up and write better tests
@@ -36,24 +61,24 @@ def test_everything():
     # print("Defaults toml file:\n" + c.generate_defaults())
 
 
-def test_validate_sources_env():
-    """Test env sources are validated properly."""
-    try:
-        from dotenv import load_dotenv
-    except ImportError:
-        pytest.skip("dotenv not installed")
+@pytest.mark.xfail(reason="Not finished.")
+@pytest.mark.dependency(depends=["load_dotenv"])
+def test_missing_config():
+    """Test an error is raised when a file does not exist."""
 
-    import configurave
-    from configurave import validate_sources
+    @make_config(
+        sources=[  # in order of priority
+            "tests/test-config/fake-config.toml",
+        ]
+    )
+    class Config:
+        """The test configuration for configurave."""
 
-    validate_sources(["ENV"])
+        root_url: str = ce(
+            comment="The root url configuration for the application",
+            description="A long ass multiline description goes here about all the options"
+            " you could potentially decide upon using.",
+        )
 
-
-def test_validate_sources_env_dotenv_not_installed():
-    """Test env source errors properly when dotenv is not installed."""
-    import configurave
-    from configurave import validate_sources
-
-    configurave.load_dotenv = None
-    with pytest.raises(ConfigError):
-        validate_sources(["ENV"])
+    c = Config()
+    c.load()
